2774c791cb813b4c942ec278f0b329bd80084170
Nikolay Kuleshov <nakuleshov@gmail.com>
2025-10-20 11:32:02 +0300

    Забытый build.gradle.

---
+++ notifier-bot-api/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+  id("java-library")
+}
+
+dependencies {
+
+  testImplementation(platform(libs.bom.junit))
+  compileOnly(libs.slf4j)
+  testImplementation(libs.junit.jupiter)
+  testRuntimeOnly(libs.junit.jupiter.launcher)
+}
+
